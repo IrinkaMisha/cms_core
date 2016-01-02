cce34396ce27e2851a49c1c619b2ec811149a1d3
Mihail Kachanovskii <nulay@mail.ru>
2016-01-02 22:30:04 +0300

    commit

---
+++ cmscore-web/src/main/java/rw/ktc/cms/prepare/firststart/InitDataBase.java
@@ -0,0 +1,98 @@
+package rw.ktc.cms.prepare.firststart;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import rw.ktc.cms.web.form.DatabaseForm;
+
+import javax.servlet.ServletContext;
+import javax.validation.Valid;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+
+public class InitDataBase {
+    private static final Logger logger = LoggerFactory.getLogger(InitDataBase.class);
+
+    @Autowired
+    ServletContext servletContext;
+
+    private final String config="<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"+
+            "<beans xmlns=\"http://www.springframework.org/schema/beans\"\n"+
+            "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n"+
+            "xsi:schemaLocation=\"http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans.xsd\">\n"+
+            "<!-- after write file -->\n"+
+            "<import resource=\"classpath:spring-web-config.xml\"/>\n"+
+            "</beans>\n";
+
+    @RequestMapping(value = "/cms.html")
+    public String createNewUser(DatabaseForm databaseForm, Model model){
+        List<String> database = new ArrayList<String>();
+        List<String> dialect =new ArrayList<String>();
+        database.add("com.mysql.jdbc.Driver");
+        database.add("com.ibm.db2.jcc.DB2Driver");
+
+        dialect.add("org.hibernate.dialect.MySQLDialect");
+        dialect.add("org.hibernate.dialect.DB2Dialect");
+
+        model.addAttribute("listDatabase", database);
+        model.addAttribute("listDialect", dialect);
+        model.addAttribute("databaseForm", databaseForm);
+        return "initDataBase";
+    }
+
+    @RequestMapping(value = "/cms/initdatabase.html", method = RequestMethod.POST)
+    public String saveNewUser(@Valid DatabaseForm databaseForm, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return "initDataBase";
+        }
+        try {
+            Class.forName(databaseForm.getDriverClassName());
+            Connection con = DriverManager.getConnection(databaseForm.getUrl(), databaseForm.getLogin(), databaseForm.getPassword());
+            con.close();
+
+            File file = new File(servletContext.getRealPath("/WEB-INF/startSpring-servlet.xml"));
+            File copyFile = new File(servletContext.getRealPath("/WEB-INF/temp.xml"));
+            FileWriter writer = new FileWriter(copyFile, false);
+            writer.write(config);
+            writer.close();
+            file.delete();
+            copyFile.renameTo(file);
+
+            Properties prop=new Properties();
+            prop.load(servletContext.getResourceAsStream("/WEB-INF/classes/jdbc.properties"));
+            prop.setProperty("org.hibernate.dialect", databaseForm.getDialect());
+            prop.setProperty("jdbc.username", databaseForm.getLogin());
+            prop.setProperty("jdbc.password", databaseForm.getPassword());
+            prop.setProperty("jdbc.url", databaseForm.getUrl());
+            prop.setProperty("jdbc.driverClassName",databaseForm.getDriverClassName());
+            prop.setProperty("rw.ktc.cms.database","1");
+            prop.store(new FileOutputStream(servletContext.getRealPath("/WEB-INF/classes/jdbc.properties")), null);
+
+            return "success";
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            logger.debug("Драйвер не найден");
+            return "initDataBase";
+        } catch (SQLException e) {
+            e.printStackTrace();
+            logger.debug("Нету соединения с базой");
+            return "initDataBase";
+        } catch (IOException e) {
+            logger.debug("IOException");
+            return "error";
+        }
+    }
+}
---
+++ cmscore-web/src/main/java/rw/ktc/cms/web/dynamiccontent/DynamicContentHandler.java
@@ -0,0 +1,83 @@
+package rw.ktc.cms.web.dynamiccontent;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import rw.ktc.cms.model.IDynamicContentService;
+import rw.ktc.cms.redirect.RedirectView;
+import rw.ktc.cms.redirect.RedirectViewService;
+import rw.ktc.cms.web.Const;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by miha on 17.10.2014.
+ */
+@Transactional(readOnly = true)
+public class DynamicContentHandler implements HandlerInterceptor {
+    private static final Logger logger = LoggerFactory.getLogger(DynamicContentHandler.class);
+
+    @Autowired
+    IDynamicContentService dynamicContentService;
+
+    @Autowired
+    private RedirectViewService redirectViewService;
+
+    public DynamicContentHandler() {
+    }
+
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse response, Object o, ModelAndView modelAndView) throws Exception {
+        if (modelAndView != null && !modelAndView.getViewName().startsWith("redirect:")) {
+            RedirectView rw = redirectViewService.checkRedirect(httpServletRequest.getRequestURI());
+            if (rw != null) {
+                dynamicContentService.load(rw.getId_nodeView());
+                DynamicContent dynContent = dynamicContentService.load(rw.getId_nodeView());
+
+                Container contC = new Container();
+                contC.setName("centerPage");
+                if (modelAndView.getViewName().contains("/")) {
+                    contC.setType("jsp");
+                } else {
+                    contC.setType("tiles");
+                }
+                contC.setContent(modelAndView.getViewName());
+                modelAndView.addObject(Const.NAME_OBJECT_FOR_CENTER_PAGE_FOR_DYNAMIC_CONTENT, contC);
+
+                modelAndView.addObject(Const.NAME_MAIN_OBJECT_FOR_DYNAMIC_CONTENT, dynContent);
+                modelAndView.setViewName(dynamicContentService.getViewNameTemplate(dynContent.getNode()));
+
+                logger.debug("{} and will be redirect to {} for path - {}", rw, modelAndView.getViewName(), httpServletRequest.getServletPath());
+            }
+        }
+    }
+
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse response, Object o, Exception e) throws Exception {
+
+    }
+
+    public IDynamicContentService getDynamicContentService() {
+        return dynamicContentService;
+    }
+
+    public void setDynamicContentService(IDynamicContentService dynamicContentService) {
+        this.dynamicContentService = dynamicContentService;
+    }
+
+    public RedirectViewService getRedirectViewService() {
+        return redirectViewService;
+    }
+
+    public void setRedirectViewService(RedirectViewService redirectViewService) {
+        this.redirectViewService = redirectViewService;
+    }
+}
+
